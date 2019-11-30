0b64aecc9c1efc08ef40db7fd0879590dce6c029
marshl <marshl@live.com.au>
2019-11-30 20:12:35 +1100

    WIP

--- SvnChart.py
+++ SvnChart.py
@@ -19,13 +19,17 @@ commits_per_user_svg_path = "commits_per_user.svg"
 color_sequence = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
                   '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
                   '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-                  '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+                  '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
 
 graph_time_delta = timedelta(days=1)
 
 
+def get_repo(url):
+    return svn.utility.get_client(url)
+
+
 def get_svn_log(url, username, password):
-    repo = svn.utility.get_client(url)
+    repo = get_repo(url)
 
     return sorted(list(repo.log_default()), key=lambda x: x.date)
 
@@ -49,10 +53,11 @@ def print_commit_total(log_entries):
         f.write(str(current_date) + "," + str(commit_count) + "\n")
 
 
-def print_commit_total_per_user(log_entries):
+def print_commit_total_per_user(log_entries, url, chart_line_changes=False):
     first_date = min(x.date for x in log_entries)
-
     current_date = first_date
+
+    repo = get_repo(url)
 
     # Create a dictionary of each author to their commit count (starting at 0)
     user_commit_dict = dict([x.author or "unknown", 0] for x in log_entries)
@@ -62,6 +67,9 @@ def print_commit_total_per_user(log_entries):
         f.write(','.join(user_commit_dict.keys()) + "\n")
 
         for entry in log_entries:
+
+            author = entry.author or "unknown"
+
             while entry.date > current_date:
                 current_date += graph_time_delta
                 f.write(str(current_date))
@@ -71,7 +79,18 @@ def print_commit_total_per_user(log_entries):
 
                 f.write("\n")
 
-            user_commit_dict[entry.author or "unknown"] += 1
+            if chart_line_changes:
+                try:
+                    print('Diffing revision {0}'.format(entry.revision))
+                    diffs = repo.diff(int(entry.revision), int(entry.revision) + 1)
+                    for diff in diffs:
+                        user_commit_dict[author] += diff['diff'].count('\n+')
+
+                except Exception as ex:
+                    print(ex)
+
+            else:
+                user_commit_dict[author] += 1
 
 
 def chart_commit_total():
@@ -123,9 +142,9 @@ def chart_commit_total_per_user():
         line_colour = color_sequence[i % len(color_sequence)]
 
         user_labels.append({'username': column_name,
-                           'commits': last_commit_count,
-                           'last_date': truncated_dates[-1],
-                           'colour': line_colour})
+                            'commits': last_commit_count,
+                            'last_date': truncated_dates[-1],
+                            'colour': line_colour})
 
         line = plt.plot(truncated_dates,
                         truncated_data,
@@ -143,27 +162,37 @@ def chart_commit_total_per_user():
 
 
 def build_option_parser():
-    parser = OptionParser()
-    parser.add_option("-u", "--username", dest="username",
-                      help="the user to connect as")
+    opt_parser = OptionParser()
+    opt_parser.add_option("-u", "--username", dest="username",
+                          help="the user to connect as")
 
-    parser.add_option("-p", "--password", dest="password",
-                      help="the password of the user")
+    opt_parser.add_option("-p", "--password", dest="password",
+                          help="the password of the user")
 
-    parser.add_option("-d", "--download", action="store_true", dest="download",
-                      help="downloads the log file even if it already exists")
+    opt_parser.add_option("-d", "--download", action="store_true", dest="download_files",
+                          help="downloads the log file even if it already exists")
 
-    return parser
+    opt_parser.add_option("-l", "--lines", action="store_true", dest="count_lines",
+                          help="count the number of line changes in each diff")
+
+    return opt_parser
 
 
 if __name__ == "__main__":
     parser = build_option_parser()
     (options, args) = parser.parse_args()
 
-    log_entries = get_svn_log(args[0], options.username, options.password)
+    svn_url = args[0]
 
-    print_commit_total(log_entries)
+    if options.download_files:
+
+        log_entries = get_svn_log(svn_url, options.username, options.password)
+
+        if options.count_lines:
+            print_commit_total_per_user(log_entries, svn_url, True)
+        else:
+            print_commit_total(log_entries)
+            print_commit_total_per_user(log_entries, svn_url, False)
+
     chart_commit_total()
-
-    print_commit_total_per_user(log_entries)
     chart_commit_total_per_user()
